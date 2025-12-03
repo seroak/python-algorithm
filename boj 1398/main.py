3c337a6fad03a577589699b0cd391ccb70c18ee9
lee gyu yeol-mac <i1004gy@gmail.com>
2025-12-03 09:47:53 +0900

    문제 풀이

--- boj 1398/main.py
+++ boj 1398/main.py
@@ -5,4 +5,14 @@ for _ in range(t):
     coins = [1, 10, 25]
     answer = 0
     dp =[10**15+1 for _ in range(100)]
+    dp[0] = 0
+    for c in [1, 10, 25]:
+        for i in range(c, 100):
+            dp[i] = min(dp[i], dp[i-c]+1)
     print(dp)
+    while cost:
+        print(cost)
+        print(dp[cost%100])
+        answer += dp[cost % 100]
+        cost //= 100
+    print(answer)
